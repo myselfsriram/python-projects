594a60a38bff3f635e2408c8b404f6d6b31ff67c
myselfsriram <97535682+myselfsriram@users.noreply.github.com>
2022-05-08 10:57:23 +0530

    First

---
+++ control.py
@@ -0,0 +1,14 @@
+from pyfirmata import  Arduino,SERVO
+port='COM5'
+pin=10
+board=Arduino(port)
+board.digital[pin].mode=SERVO
+
+def rotateServo(pin,angle):
+    board.digital[pin].write(angle)
+
+def doorAutomate(val):
+    if val==0:
+        rotateServo(pin,180)
+    elif val==1:
+        rotateServo(pin,40)
